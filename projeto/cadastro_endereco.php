0c904e883ba4c7dd25085e350a3be4df2bfa3c02
lvsodre07 <128701738+lvsodre07@users.noreply.github.com>
2024-12-09 17:52:22 +0000

    correçao cep automatico

--- projeto/cadastro_endereco.php
+++ projeto/cadastro_endereco.php
@@ -73,6 +73,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
 </head>
 <body>
     <header>
